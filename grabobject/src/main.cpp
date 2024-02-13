c6822afcc79fb1ac88d0a95519224502ff6262a4
Hunaid Hameed <hhameed@dpz.eu>
2024-02-13 17:26:05 +0000

    ip add and comm protocol update

--- grabobject/src/main.cpp
+++ grabobject/src/main.cpp
@@ -7,7 +7,7 @@
 
 int main(int argc, char **argv)
 {
-  uint8_t msg;
+  uint16_t msg;
   pthread_t ptid;
 
   try
@@ -16,10 +16,10 @@ int main(int argc, char **argv)
 
     sockpp::initialize();
     sockpp::udp_socket sock_position_command, sock_grasp_state;
-    sockpp::inet_address addr("localhost", 1000); // listener address for behaviour computer
+    sockpp::inet_address addr("192.168.137.20", 1000); // listener address for behaviour computer
 
     // speaker socket to hand position
-    if (auto err = sock_position_command.bind(sockpp::inet_address("localhost", 1400)))
+    if (auto err = sock_position_command.bind(sockpp::inet_address("192.168.137.40", 4700)))
     {
       std::cerr << "UDP socket bind: " << err.error_message() << std::endl;
     }
@@ -29,6 +29,9 @@ int main(int argc, char **argv)
       auto n = sock_position_command.recv(&msg, sizeof(msg), MSG_DONTWAIT);
 
       arm.gripObject(msg);
+
+      // the last 7 most significant bits out of total 16 are the extent of the reach
+      msg = msg >> 9;
 
       arm.updateState();
       
